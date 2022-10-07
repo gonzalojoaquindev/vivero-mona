c0a2e49e11027a4c2e8acc0b14b364bc34174148
gonzaloJoaquin <58834360+gonzalojoaquindev@users.noreply.github.com>
2022-10-07 13:19:56 -0300

    Add files via upload

---
+++ precache-manifest.4ee6309b8a8fe2d733fa18fd651ebcab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62646121e40fea7d3e74",
+    "url": "/css/chunk-vendors.5119d5ae.css"
+  },
+  {
+    "revision": "2eb3e30bc6e3a66b56f377462bfc196f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fd71084203a8934b84f",
+    "url": "/js/about.625777a2.js"
+  },
+  {
+    "revision": "4863d7c270e50ee45832",
+    "url": "/js/app.ad80802c.js"
+  },
+  {
+    "revision": "62646121e40fea7d3e74",
+    "url": "/js/chunk-vendors.2978b5a4.js"
+  },
+  {
+    "revision": "bcece312d1a93cfd43972ed203239f0b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
